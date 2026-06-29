5d3806b52ddff628f5fb7aad8429da0b4a6881eb
kaetisak <104181671+kaetisak@users.noreply.github.com>
2026-06-29 15:07:59 +0700

    feat(assignment): add basic arithmetic functions in Lecture50_Kaetisak_W.py

---
+++ assignments/Lecture50_Kaetisak_W.py
@@ -0,0 +1,16 @@
+def add_number(x, y):
+    print(x+y)
+
+def sub_number(x, y):
+    print(x-y)
+
+def mul_number(x, y):
+    print(x*y)
+
+def div_number(x, y):
+    print(x/y)
+
+add_number(100, 25)
+sub_number(50, 20)
+mul_number(10, 5)
+div_number(9, 3)
