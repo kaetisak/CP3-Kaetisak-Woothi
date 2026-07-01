674aa992bda9e5c18582b2546d583f583e42a077
kaetisak <104181671+kaetisak@users.noreply.github.com>
2026-07-01 12:54:33 +0700

    feat(Exercise21_Kaetisak_W.py): add BMI calculator application using tkinter

---
+++ assignments/Exercise21_Kaetisak_W.py
@@ -0,0 +1,32 @@
+from tkinter import *
+import math
+
+def leftClickButton(event):
+    bmi = float(textBoxWeight.get())/math.pow(float(textBoxHeight.get())/100,2)
+    if bmi >= 30:
+        labelResult.configure(text="อ้วนมาก")
+    elif bmi >= 25:
+        labelResult.configure(text="อ้วน")
+    elif bmi >= 23:
+        labelResult.configure(text="น้ำหนักเกิน")
+    elif bmi >= 18.6:
+        labelResult.configure(text="น้ำหนักปกติ เหมาะสม")
+    else:
+        labelResult.configure(text="ผอมเกินไป")
+
+MainWindow = Tk()
+labelHeight = Label(MainWindow, text="ส่วนสูง (cm.)")
+labelHeight.grid(row=0,column=0)
+textBoxHeight = Entry(MainWindow)
+textBoxHeight.grid(row=0,column=1)
+labelWeigth = Label(MainWindow, text="น้ำหนัก (Kg.)")
+labelWeigth.grid(row=1,column=0)
+textBoxWeight = Entry(MainWindow)
+textBoxWeight.grid(row=1,column=1)
+calculateButton = Button(MainWindow,text = "คำนวน")
+calculateButton.bind('<Button-1>', leftClickButton)
+calculateButton.grid(row=2)
+labelResult = Label(MainWindow,text="ผลลัพธ์")
+labelResult.grid(row=2,column=1)
+
+MainWindow.mainloop()
